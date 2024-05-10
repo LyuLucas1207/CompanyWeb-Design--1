5317741a9b26f600d9af9f104cf29208e17218aa
Lyu <lyuchongkai@gmail.com>
2024-05-10 12:21:06 -0700

    2024-05-10-12-21

--- js/section12.js
+++ js/section12.js
@@ -1,12 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    loadDataAndCreateElements('./data/section12/section12_1.json');
-    loadDataAndCreateElements('./data/section12/section12_2.json');
-    loadDataAndCreateElements('./data/section12/section12_3.json');
-    loadDataAndCreateElements('./data/section12/section12_4.json');
-    loadDataAndCreateElements('./data/section12/section12_5.json');
-});
-
-
 //when not use async
 // document.addEventListener('DOMContentLoaded', function() {loadDataAndCreateElements('./data/section12/section12_1.json');});
 // document.addEventListener('DOMContentLoaded', function() {loadDataAndCreateElements('./data/section12/section12_2.json');});
@@ -16,68 +7,97 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-async function loadDataAndCreateElements(url) {
-    const container = document.querySelector('.section12_card_container');
 
+// document.addEventListener('DOMContentLoaded', function() {
+//     const container = document.querySelector('.section12_card_container');
+//     fetch('./data/section12/section12.json')
+//         .then(response => response.json())
+//         .then(data => {
+//             const categories = Object.keys(data);
+
+//             categories.forEach(category => {
+//                 const categoryDiv = document.createElement('div');
+//                 categoryDiv.className = 'section12_category';
+
+//                 const titleDiv = document.createElement('div');
+//                 titleDiv.className = 'section12_title';
+
+//                 titleDiv.innerHTML = `<h1 id="${category}">${category}</h1><p>各种产品任意选择</p>`;
+//                 categoryDiv.appendChild(titleDiv);
+
+//                 const cardDiv = document.createElement('div');
+//                 cardDiv.className = 'section12_cards';
+//                 cardDiv.id = `section12_cards_${category}`;
+//                 categoryDiv.appendChild(cardDiv);
+//                 container.appendChild(categoryDiv);
+
+//             });
+//         })
+//         .catch(error => console.error(error));
+// });
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadDataAndCreateElements('./data/section12/section12_1.json');
+    loadDataAndCreateElements('./data/section12/section12_2.json');
+    loadDataAndCreateElements('./data/section12/section12_3.json');
+    loadDataAndCreateElements('./data/section12/section12_4.json');
+    loadDataAndCreateElements('./data/section12/section12_5.json');
+    changeParagraph('./data/section12/section12.json');
+});
+
+async function changeParagraph(url) {
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const categories = Object.keys(data);
+            categories.forEach(category => {
+                const para = document.getElementById(`p_${category}`);
+                if (para) {  // Check if the paragraph element exists
+                    para.innerHTML = data[category][0].paragraph;
+                } else {
+                    console.error(`No paragraph with id p_${category} found.`);
+                }
+            });
+        })
+        .catch(error => console.error('Error loading the paragraph data:', error));
+}
+
+
+async function loadDataAndCreateElements(url) {
+    const cards = document.querySelector('.section12_cards');
     fetch(url)
         .then(response => response.json())
         .then(data => {
             const categories = Object.keys(data);
-
             categories.forEach(category => {
-                const categoryDiv = document.createElement('div');
-                categoryDiv.className = 'section12_category';
-
-                const titleDiv = document.createElement('div');
-                titleDiv.className = 'section12_title';
-                titleDiv.innerHTML = `<h1>${category}</h1><p>各种产品任意选择</p>`;
-                categoryDiv.appendChild(titleDiv);
-
-                const cardDiv = document.createElement('div');
-                cardDiv.className = 'section12_cards';
-
-                data[category].forEach(product => {
-                    const card = createCard(product);
-                    cardDiv.appendChild(card);
-                });
-
-                categoryDiv.appendChild(cardDiv);
-                container.appendChild(categoryDiv);
+            
+                if(document.getElementById(`section12_cards_${category}`)){
+                    data[category].forEach(product => {
+                        const id = `section12_cards_${category}`;
+                        const cardsID = document.getElementById(id);
+                        const card = createCard(product);
+                        if (cardsID) {
+                            cardsID.appendChild(card);
+                        } else {
+                            console.error(`No element with id ${id} found.`);
+                        }
+                    });
+                    cards.appendChild(cardsID);
+                }else{
+                    console.error(`No element with id section12_cards_${category} found.`);
+                }
             });
         })
-        .catch(error => {
-            console.error('Error loading the products data:', error);
-        });
+        .catch(error => console.error('Error loading the products data:', error));
 }
-
-// function createCard(product) {
-//     const card = document.createElement('div');
-//     card.className = 'section12_card';
-//     card.innerHTML = `
-//         <div class="section12_loader"><img src="./pic/data/loading.gif" alt="Loading..."></div>
-//         <a href="#" style="display: none;">
-//             <img src="${product.img}" alt="${product.name}">
-//             <div class="section12_card_text">
-//                 <h3>${product.name}</h3>
-//                 <p>${product.description}</p>
-//                 <p>price:￥${product.price}</p>
-//             </div>
-//         </a>
-//     `;
-
-//     // Image loading management
-//     const img = card.querySelector('img');
-//     img.onload = () => {
-//         card.querySelector('a').style.display = 'flex'; // Show the card content
-//         card.querySelector('.section12_loader').style.display = 'none'; // Hide the section12_loader
-//     };
-//     img.onerror = () => {
-//         const loader = card.querySelector('.section12_loader');
-//         loader.innerHTML = '<p>Error loading image.</p>'; // Show error message
-//     };
-
-//     return card;
-// }
 
 
 function createCard(product) {
@@ -104,7 +124,10 @@ function createCard(product) {
     const img = card.querySelector('img');
     img.onload = () => {
         card.querySelector('a').style.display = 'flex';
-        card.querySelector('.section12_loader').style.display = 'none';
+        // card.querySelector('.section12_loader').style.display = 'none';
+        card.querySelectorAll('.section12_loader').forEach(loader => {
+            loader.style.display = 'none';
+        });
     };
     img.onerror = () => {
         const loader = card.querySelector('.section12_loader');
@@ -137,7 +160,125 @@ function showModal(product) {
     };
 }
 
+/*
+    <div class="section12">
+        <div class="section12_card_container">
+    
+            <!-- 动态内容将被JavaScript脚本填充 --->
+        </div>
+    
+        <!-- Modal example -->
+        <div id="modal1" class="section12_modal">
+            <div class="section12_modal-content">
+                <!-- 模态内容会在 JavaScript 中动态填充 -->
+            </div>
+        </div>
+    </div>
 
+
+
+
+*/
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     loadDataAndCreateElements('./data/section12/section12_1.json');
+//     loadDataAndCreateElements('./data/section12/section12_2.json');
+//     loadDataAndCreateElements('./data/section12/section12_3.json');
+//     loadDataAndCreateElements('./data/section12/section12_4.json');
+//     loadDataAndCreateElements('./data/section12/section12_5.json');
+// });
+
+
+// async function loadDataAndCreateElements(url) {
+//     const container = document.querySelector('.section12_card_container');
+
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => {
+//             const categories = Object.keys(data);
+
+//             categories.forEach(category => {
+//                 const categoryDiv = document.createElement('div');
+//                 categoryDiv.className = 'section12_category';
+
+//                 const titleDiv = document.createElement('div');
+//                 titleDiv.className = 'section12_title';
+//                 titleDiv.innerHTML = `<h1 id="${category}">${category}</h1><p>各种产品任意选择</p>`;
+//                 categoryDiv.appendChild(titleDiv);
+
+//                 const cardDiv = document.createElement('div');
+//                 cardDiv.className = 'section12_cards';
+
+//                 data[category].forEach(product => {
+//                     const card = createCard(product);
+//                     cardDiv.appendChild(card);
+//                 });
+
+//                 categoryDiv.appendChild(cardDiv);
+//                 container.appendChild(categoryDiv);
+//             });
+//         })
+//         .catch(error => {
+//             console.error('Error loading the products data:', error);
+//         });
+// }
+
+// function createCard(product) {
+//     const card = document.createElement('div');
+//     card.className = 'section12_card';
+//     card.innerHTML = `
+//         <div class="section12_loader"><img src="./pic/data/loading.gif" alt="Loading..."></div>
+//         <a href="#" style="display: none;">
+//             <img src="${product.img}" alt="${product.name}">
+//             <div class="section12_card_text">
+//                 <h3>${product.name}</h3>
+//                 <p>${product.description}</p>
+//                 <p>price:￥${product.price}</p>
+//             </div>
+//         </a>
+//     `;
+
+//     card.querySelector('a').addEventListener('click', function(event) {
+//         event.preventDefault();
+//         showModal(product);
+//     });
+
+//     // Image loading management
+//     const img = card.querySelector('img');
+//     img.onload = () => {
+//         card.querySelector('a').style.display = 'flex';
+//         card.querySelector('.section12_loader').style.display = 'none';
+//     };
+//     img.onerror = () => {
+//         const loader = card.querySelector('.section12_loader');
+//         loader.innerHTML = '<p>Error loading image.</p>';
+//     };
+
+//     return card;
+// }
+
+// function showModal(product) {
+//     const modal = document.querySelector('.section12_modal');
+//     const modalContent = modal.querySelector('.section12_modal-content');
+//     modalContent.innerHTML = `
+//         <span class="section12_close">&times;</span>
+//         <h2>${product.name}</h2>
+//         <img src="${product.img}" alt="${product.name}">
+//         <p>${product.description}</p>
+//         <p>￥${product.price}</p>
+//         <p>${product.detailed_info}</p>
+//     `;
+//     modal.style.display = 'block';
+//     modal.querySelector('.section12_close').addEventListener('click', function() {
+//         modal.style.display = 'none';
+//     });
+//     //when click outside the modal, close the modal
+//     window.onclick = function(event) {
+//         if (event.target === modal) {
+//             modal.style.display = 'none';
+//         }
+//     };
+// }
 
 // document.addEventListener("DOMContentLoaded", function() {
 //     var triggers = document.querySelectorAll('.section12_cards .section12_card');
